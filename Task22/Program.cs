f140b129be0d014a9cdf37b16eeaee8bfb189c2e
Kommissar Egro <ugalovnik666@gmil.com>
2023-08-01 15:02:03 +1000

    решение задачи 22

--- Task22/Program.cs
+++ Task22/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу,которая принимает на вход число (N)
+// и выдает таблицу квадратов чисел от  до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1, 4
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1)
+{
+    Console.WriteLine("Некоректные данные");
+    return;
+}
+SquareTable(number);
+
+void SquareTable(int num)
+{
+    for (int i = 0; i <= num; i++)
+    {
+        int square = i * i;
+        Console.WriteLine($"{i}    {square}");
+    }
+
+}
