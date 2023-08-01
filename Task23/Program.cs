de16a626e0a242b89dd8a736b11569e559ce8dca
Kommissar Egro <ugalovnik666@gmil.com>
2023-08-01 20:11:15 +1000

    Решение задачи 23

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N)
+// и выдает таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1)
+{
+    Console.WriteLine("Некоректные данные");
+    return;
+}
+SquareTable(number);
+
+void SquareTable(int num)
+{
+    for (int i = 0; i <= num; i++)
+    {
+        int square = (i * i) * i ;
+        Console.WriteLine($"{i, 3}{square, 5}");
+    }
+
+}
