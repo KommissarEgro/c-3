d8d60b868b384b66df154a2a87c43208e0da82fd
Kommissar Egro <ugalovnik666@gmil.com>
2023-08-01 13:25:59 +1000

    Решение задачи 18

--- Task18/Program.cs
+++ Task18/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая по заданному номеру четверти,
+// показывает диапазон возможных координат точек в этой четверти (x,y).
+
+
+Console.WriteLine("Введите номер четверти ");
+string numberQuarter = Console.ReadLine();
+
+string range = RangeQuarter(numberQuarter);
+if (range != null)
+    Console.WriteLine(range);
+else Console.WriteLine("Неккоректная четверть");
+
+string RangeQuarter(string numberQuarter)
+{
+    if (numberQuarter == "1") return "x > 0 && y > 0";
+    if (numberQuarter == "2") return "x < 0 && y > 0";
+    if (numberQuarter == "3") return "x < 0 && y < 0";
+    if (numberQuarter == "4") return "x > 0 && y < 0";
+    return null;
+}
